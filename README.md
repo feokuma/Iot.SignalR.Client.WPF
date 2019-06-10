9f50c48b41cf041fdffd3c12e190b7af1027bee3
Fernando Okuma <feokuma@gmail.com>
2019-06-09 22:31:19 -0300

    first commit

---
+++ README.md
@@ -0,0 +1,1 @@
+# Iot.SignalR.Client.WPF
---
+++ src/MainWindow.xaml.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Windows;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.SignalR.Client;
+
+namespace Iot.SignalR.Client
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        private HubConnection hubConnection;
+        public MainWindow()
+        {
+            InitializeComponent();
+
+            hubConnection = new HubConnectionBuilder()
+                .WithUrl("https://localhost:5001/IotServerHub")
+                .Build();
+
+            hubConnection.Closed += async (error) =>
+            {
+                await Task.Delay(new Random().Next(0, 5) * 1000);
+                await hubConnection.StartAsync();
+            };
+        }
+
+        private void BtnConnect_Click(object sender, RoutedEventArgs e)
+        {
+            hubConnection.On<string>("SetState", (message) =>
+            {
+                this.Dispatcher.Invoke(() =>
+                {
+                    lstMessages.Items.Add(message);
+                });
+            });
+
+            hubConnection.StartAsync();
+        }
+
+        private void BtnSendMessage_Click(object sender, RoutedEventArgs e)
+        {
+            hubConnection.SendAsync("SendState", txtMessage.Text);
+        }
+    }
+}
